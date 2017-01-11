34e0ed65ea3c216d51a68a5ac2e7734bf45ef014
GaelCoat <zoul.gaelcoat@gmail.com>
2017-01-11 06:17:33 +0100

    swip

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -10,11 +10,11 @@ webpackJsonp([0],[
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(_, Backbone, $, q) {var Home = __webpack_require__(9);
-	var Menu = __webpack_require__(16);
-	var Video = __webpack_require__(10);
-	var Stats = __webpack_require__(11);
-	var Lightbox = __webpack_require__(12);
-	var Salvattore = __webpack_require__(13);
+	var Menu = __webpack_require__(11);
+	var Video = __webpack_require__(12);
+	var Stats = __webpack_require__(13);
+	var Lightbox = __webpack_require__(15);
+	var Salvattore = __webpack_require__(16);
 
 	_.templateSettings = {
 	  interpolate: /\{\{(.+?)\}\}/g,
@@ -187,7 +187,7 @@ webpackJsonp([0],[
 /* 9 */
 /***/ function(module, exports, __webpack_require__) {
 
-	/* WEBPACK VAR INJECTION */(function(Backbone, _, $) {var isMobile = __webpack_require__(14);
+	/* WEBPACK VAR INJECTION */(function(Backbone, _, $) {var isMobile = __webpack_require__(10);
 
 	module.exports = Backbone.View.extend({
 
@@ -229,6 +229,56 @@ webpackJsonp([0],[
 
 /***/ },
 /* 10 */
+/***/ function(module, exports) {
+
+	module.exports = function(a){ return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))}(navigator.userAgent||navigator.vendor||window.opera);
+
+
+/***/ },
+/* 11 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(Backbone, _, $) {var isMobile = __webpack_require__(10);
+
+	module.exports = Backbone.View.extend({
+
+	  events: {},
+
+	  initialize: function(params) {
+
+	  },
+
+	  renderWeb: function() {
+
+	    var tpl = _.template($('#tpl-menu-web').html());
+
+	    this.$el.prepend(tpl());
+	    return this;
+	  },
+
+	  renderMobile: function() {
+
+	    var tpl = _.template($('#tpl-menu-mobile').html());
+
+	    this.$el.prepend(tpl());
+	    return this;
+	  },
+
+	  render: function() {
+
+	    if (isMobile) return this.renderMobile();
+	    else return this.renderWeb();
+	    return this;
+	  },
+
+	})
+
+
+
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(3), __webpack_require__(2), __webpack_require__(4)))
+
+/***/ },
+/* 12 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(Backbone, _, $) {
@@ -289,10 +339,10 @@ webpackJsonp([0],[
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(3), __webpack_require__(2), __webpack_require__(4)))
 
 /***/ },
-/* 11 */
+/* 13 */
 /***/ function(module, exports, __webpack_require__) {
 
-	/* WEBPACK VAR INJECTION */(function(Backbone, _, $) {var stats = __webpack_require__(15);
+	/* WEBPACK VAR INJECTION */(function(Backbone, _, $) {var stats = __webpack_require__(14);
 
 	module.exports = Backbone.View.extend({
 
@@ -362,7 +412,107 @@ webpackJsonp([0],[
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(3), __webpack_require__(2), __webpack_require__(4)))
 
 /***/ },
-/* 12 */
+/* 14 */
+/***/ function(module, exports) {
+
+	module.exports = {
+
+	  seasons: {
+
+	    2012: {
+
+	      games: {
+
+	        total: 35,
+	        l1: 32,
+	        coupe: 3,
+	      },
+
+	      actions: {
+
+	        total: 11,
+	        goals: 10,
+	        passes: 1
+	      }
+	    },
+
+	    2013: {
+
+	      games: {
+
+	        total: 41,
+	        l1: 31,
+	        c1: 6,
+	        coupe: 4,
+	      },
+
+	      actions: {
+
+	        total: 14,
+	        goals: 10,
+	        passes: 4
+	      }
+	    },
+
+	    2014: {
+
+	      games: {
+
+	        total: 38,
+	        l1: 36,
+	        coupe: 2,
+	      },
+
+	      actions: {
+
+	        total: 9,
+	        goals: 5,
+	        passes: 4
+	      }
+	    },
+
+	    2015: {
+
+	      games: {
+
+	        total: 38,
+	        l1: 16,
+	        pl: 13,
+	        coupe: 9,
+	      },
+
+	      actions: {
+
+	        total: 8,
+	        goals: 5,
+	        passes: 3
+	      }
+	    },
+
+	    2016: {
+
+	      games: {
+
+	        total: 21,
+	        l1: 19,
+	        coupe: 2,
+	      },
+
+	      actions: {
+
+	        total: 9,
+	        goals: 5,
+	        passes: 4
+	      }
+	    },
+
+	  }
+
+	}
+
+
+/***/ },
+/* 15 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(Backbone, _, $) {
@@ -422,7 +572,7 @@ webpackJsonp([0],[
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(3), __webpack_require__(2), __webpack_require__(4)))
 
 /***/ },
-/* 13 */
+/* 16 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -1029,156 +1179,6 @@ webpackJsonp([0],[
 	return salvattore;
 	}));
 
-
-/***/ },
-/* 14 */
-/***/ function(module, exports) {
-
-	module.exports = function(a){ return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))}(navigator.userAgent||navigator.vendor||window.opera);
-
-
-/***/ },
-/* 15 */
-/***/ function(module, exports) {
-
-	module.exports = {
-
-	  seasons: {
-
-	    2012: {
-
-	      games: {
-
-	        total: 35,
-	        l1: 32,
-	        coupe: 3,
-	      },
-
-	      actions: {
-
-	        total: 11,
-	        goals: 10,
-	        passes: 1
-	      }
-	    },
-
-	    2013: {
-
-	      games: {
-
-	        total: 41,
-	        l1: 31,
-	        c1: 6,
-	        coupe: 4,
-	      },
-
-	      actions: {
-
-	        total: 14,
-	        goals: 10,
-	        passes: 4
-	      }
-	    },
-
-	    2014: {
-
-	      games: {
-
-	        total: 38,
-	        l1: 36,
-	        coupe: 2,
-	      },
-
-	      actions: {
-
-	        total: 9,
-	        goals: 5,
-	        passes: 4
-	      }
-	    },
-
-	    2015: {
-
-	      games: {
-
-	        total: 38,
-	        l1: 16,
-	        pl: 13,
-	        coupe: 9,
-	      },
-
-	      actions: {
-
-	        total: 8,
-	        goals: 5,
-	        passes: 3
-	      }
-	    },
-
-	    2016: {
-
-	      games: {
-
-	        total: 21,
-	        l1: 19,
-	        coupe: 2,
-	      },
-
-	      actions: {
-
-	        total: 9,
-	        goals: 5,
-	        passes: 4
-	      }
-	    },
-
-	  }
-
-	}
-
-
-/***/ },
-/* 16 */
-/***/ function(module, exports, __webpack_require__) {
-
-	/* WEBPACK VAR INJECTION */(function(Backbone, _, $) {var isMobile = __webpack_require__(14);
-
-	module.exports = Backbone.View.extend({
-
-	  events: {},
-
-	  initialize: function(params) {
-
-	  },
-
-	  renderWeb: function() {
-
-	    var tpl = _.template($('#tpl-menu-web').html());
-
-	    this.$el.prepend(tpl());
-	    return this;
-	  },
-
-	  renderMobile: function() {
-
-	    var tpl = _.template($('#tpl-menu-mobile').html());
-
-	    this.$el.prepend(tpl());
-	    return this;
-	  },
-
-	  render: function() {
-
-	    if (isMobile) return this.renderMobile();
-	    else return this.renderWeb();
-	    return this;
-	  },
-
-	})
-
-
-
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(3), __webpack_require__(2), __webpack_require__(4)))
 
 /***/ }
 ]);
